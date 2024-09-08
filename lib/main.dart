0ee723d6e2ad703be1867309fe7c1eebe082b21a
EulogiusKurdeoH <220711689@students.uajy.ac.id>
2024-09-09 06:33:11 +0700

    Push ke 2

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page Hesay'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page Kurdeo Hesay'),
     );
   }
 }
